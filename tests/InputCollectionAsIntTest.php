d9b4b63a1c7192555c351f314277ba6a1e5e437b
David Lienhard <dl@digitalmaterial.ch>
2023-10-10 22:31:47 +0200

    update

--- tests/InputCollectionAsIntTest.php
+++ tests/InputCollectionAsIntTest.php
@@ -111,4 +111,71 @@ class InputCollectionAsIntTest extends TestCase
         $collection = new InputCollection([]);
         $this->assertNull($collection->asNullableInt("doesnotexist"));
     }
+
+    /**
+     * @covers \DavidLienhard\InputWrapper\InputCollection
+     * @test
+    */
+    public function testAsNullableIntThrowsOnArray(): void
+    {
+        $collection = new InputCollection([ "key" => []]);
+        $this->expectException(InputWrapperException::class);
+        $collection->asNullableInt("key");
+    }
+
+    /**
+     * @covers \DavidLienhard\InputWrapper\InputCollection
+     * @test
+    */
+    public function testAsNullableIntReturnsDataAsInt(): void
+    {
+        $testData = [
+            [
+                "input"    => 1,
+                "expected" => 1
+            ],
+            [
+                "input"    => 0,
+                "expected" => 0
+            ],
+            [
+                "input"    => -5,
+                "expected" => -5
+            ],
+            [
+                "input"    => 10,
+                "expected" => 10
+            ],
+            [
+                "input"    => 0.5,
+                "expected" => 0
+            ],
+            [
+                "input"    => 10.4,
+                "expected" => 10
+            ],
+            [
+                "input"    => true,
+                "expected" => 1
+            ],
+            [
+                "input"    => false,
+                "expected" => 0
+            ],
+            [
+                "input"    => null,
+                "expected" => null
+            ],
+            [
+                "input"    => "test",
+                "expected" => 0
+            ]
+        ];
+
+        foreach ($testData as $case) {
+            $collection = new InputCollection([ "key" => $case['input'] ]);
+            $value = $collection->asNullableInt("key");
+            $this->assertEquals($case['expected'], $value, "given input: ".$case['input']);
+        }
+    }
 }
